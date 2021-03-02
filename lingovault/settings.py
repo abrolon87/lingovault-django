cab3eb9017dfd74d770056f071713d2de4b33380
Amanda Rolon <abrolon87@gmail.com>
2021-03-02 15:03:50 -0500

    adds lingovault_app to INSTALLED_APPS

--- lingovault/settings.py
+++ lingovault/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # custom apps
+    'lingovault_app',
+    # default django apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
