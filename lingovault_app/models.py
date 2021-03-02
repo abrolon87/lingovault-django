ac902e01c00cd878b1fd8829fc7a58d45b96dd6a
Amanda Rolon <abrolon87@gmail.com>
2021-03-02 14:21:50 -0500

    builds Language model

--- lingovault_app/models.py
+++ lingovault_app/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Language(models.Model):
+  name = models.CharField(max_length=20)
+  date_added = models.DateTimeField(auto_now_add=True)
+
+  def __str__(self):
+    return self.name
+
+
+    
+     
+
+
+
+
+
+
+
+
