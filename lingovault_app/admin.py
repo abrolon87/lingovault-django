90bbc93c2ac3c60b708a7cf3969e8d275974cdf3
Amanda Rolon <abrolon87@gmail.com>
2021-03-07 20:52:20 -0500

    registers Post model with admin site

--- lingovault_app/admin.py
+++ lingovault_app/admin.py
@@ -1,17 +1,7 @@
 from django.contrib import admin
 from .models import Language
+from .models import Post
 # Register your models here.
 
 admin.site.register(Language)
-
-
-
-
-
-
-
-
-
-
-
-
+admin.site.register(Post)
