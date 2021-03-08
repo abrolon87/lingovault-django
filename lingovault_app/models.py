92431df0e8dc75ea6aa406d150a9572894ce580c
Amanda Rolon <abrolon87@gmail.com>
2021-03-07 20:51:42 -0500

    builds Post model

--- lingovault_app/models.py
+++ lingovault_app/models.py
@@ -1,21 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+
 class Language(models.Model):
-  name = models.CharField(max_length=20)
-  date_added = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=20)
+    date_added = models.DateTimeField(auto_now_add=True)
 
-  def __str__(self):
-    return self.name
-
-
-    
-     
+    def __str__(self):
+        return self.name
 
 
+class Post(models.Model):
+    language = models.ForeignKey(Language, on_delete=models.CASCADE)
+    text = models.CharField(max_length=1000)
+    date_added = models.DateTimeField(auto_now_add=True)
 
-
-
-
-
-
+    def __str__(self):
+        return f"{self.text[:50]}..."
