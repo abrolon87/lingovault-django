15ea6f0ed548f92e16e77237a315ab99021337b9
Amanda Rolon <abrolon87@gmail.com>
2021-03-15 17:23:52 -0400

    builds languages view

--- lingovault_app/views.py
+++ lingovault_app/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 
+from .models import Language
 # Create your views here.
 
 
 def home(request):
     return render(request, 'lingovault_app/home.html')
+
+
+def languages(request):
+    languages = Language.objects.order_by('date_added')
+    context = {'languages': languages}
+    return render(request, 'lingovault_app/languages.html', context)
