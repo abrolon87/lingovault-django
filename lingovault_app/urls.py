2f50540fe276f9dfa5dd8ea6c2177aff9e12ae10
Amanda Rolon <abrolon87@gmail.com>
2021-03-15 20:36:55 -0400

    adds path for individual language

--- lingovault_app/urls.py
+++ lingovault_app/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     # all languages
     path('languages/', views.languages, name='languages'),
+    # page for individual language
+    path('languages/<int:language_id>/', views.language, name='language'),
 ]
