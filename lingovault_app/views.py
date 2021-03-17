9fe25c975d969d63329393a7005ea1ab8be99ff4
Amanda Rolon <abrolon87@gmail.com>
2021-03-17 19:47:49 -0400

    builds new_language view function

--- lingovault_app/views.py
+++ lingovault_app/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from .models import Language
+from .forms import LanguageForm
 # Create your views here.
 
 
@@ -19,3 +20,15 @@ def language(request, language_id):
     posts = language.post_set.order_by('-date_added')
     context = {'language': language, 'posts': posts}
     return render(request, 'lingovault_app/language.html', context)
+
+
+def new_language(request):
+    if request.method != 'POST':
+        form = LanguageForm()
+    else:
+        form = LanguageForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('lingovault_app:languages')
+    context = {'form': form}
+    return render(request, 'lingovault_app/new_language.html', context)
