3607545ab9395c4599800e752d56cd4a8b2fb4d5
Amanda Rolon <abrolon87@gmail.com>
2021-03-22 22:29:14 -0400

    adds path to new_post

--- lingovault_app/urls.py
+++ lingovault_app/urls.py
@@ -13,4 +13,6 @@ urlpatterns = [
     path('languages/<int:language_id>/', views.language, name='language'),
     # new language page
     path('new_language/', views.new_language, name='new_language'),
+    # new post
+    path('new_post/<int:language_id>/', views.new_post, name='new_post'),
 ]
