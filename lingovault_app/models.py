8df347b9c890bfca68857d181a279ff5ae4a4f13
Amanda Rolon <abrolon87@gmail.com>
2021-03-26 19:39:55 -0400

    adds ForeignKey to Language in order to belong to a user; creates migration to add foreign key column to Language

--- lingovault_app/models.py
+++ lingovault_app/models.py
@@ -1,11 +1,12 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 
 
 class Language(models.Model):
     name = models.CharField(max_length=20)
     date_added = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
