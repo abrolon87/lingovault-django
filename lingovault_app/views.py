06e1588fc65305a2331f6224d2393f9b9136e105
Amanda Rolon <abrolon87@gmail.com>
2021-03-26 20:52:06 -0400

    connects language to user; adds checks to verify current user

--- lingovault_app/views.py
+++ lingovault_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.http import Http404
 
 from .models import Language, Post
 from .forms import LanguageForm, PostForm
@@ -12,7 +13,8 @@ def home(request):
 
 @login_required
 def languages(request):
-    languages = Language.objects.order_by('date_added')
+    languages = Language.objects.filter(
+        user=request.user).order_by('date_added')
     context = {'languages': languages}
     return render(request, 'lingovault_app/languages.html', context)
 
@@ -20,6 +22,9 @@ def languages(request):
 @login_required
 def language(request, language_id):
     language = Language.objects.get(id=language_id)
+    if language.user != request.user:
+        raise Http404
+
     posts = language.post_set.order_by('-date_added')
     context = {'language': language, 'posts': posts}
     return render(request, 'lingovault_app/language.html', context)
@@ -32,7 +37,9 @@ def new_language(request):
     else:
         form = LanguageForm(data=request.POST)
         if form.is_valid():
-            form.save()
+            new_language = form.save(commit=False)
+            new_language.user = request.user
+            new_language.save()
             return redirect('lingovault_app:languages')
     context = {'form': form}
     return render(request, 'lingovault_app/new_language.html', context)
@@ -41,7 +48,8 @@ def new_language(request):
 @login_required
 def new_post(request, language_id):
     language = Language.objects.get(id=language_id)
-
+    if language.user != request.user:
+        raise Http404
     if request.method != 'POST':
         form = PostForm()
     else:
@@ -60,6 +68,8 @@ def new_post(request, language_id):
 def edit_post(request, post_id):
     post = Post.objects.get(id=post_id)
     language = post.language
+    if language.user != request.user:
+        raise Http404
 
     if request.method != 'POST':
         form = PostForm(instance=post)
