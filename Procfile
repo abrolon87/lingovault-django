1a651429f6a82ee385cd948aa84d61d7cc0c0f07
Amanda Rolon <abrolon87@gmail.com>
2021-04-03 21:45:36 -0400

    prepares for deploy

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn lingovault.wsgi --log-file -
--- lingovault/settings.py
+++ lingovault/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -129,3 +130,7 @@ STATIC_URL = '/static/'
 
 # custom settings
 LOGIN_URL = 'users:login'
+
+
+# heroku settings
+django_heroku.settings(locals())
